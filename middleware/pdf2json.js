6dcc2146585f56be8e47801900626753634e646c
Justin Joshi <JustinJoshi@proton.me>
2025-07-07 00:09:38 -0400

    updated version of bub

---
+++ middleware/pdf2json.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const PDFParser = require("pdf2json");
+const Pdf = require("../models/Pdf");
+
+const pdfParser = new PDFParser();
+
+pdfParser.on("pdfParser_dataError", (errData) =>
+ console.error(errData.parserError)
+);
+pdfParser.on("pdfParser_dataReady", (pdfData) => {
+ fs.writeFile(
+  "./pdf2json/test/F1040EZ.json",
+  JSON.stringify(pdfData),
+  (data) => console.log(data)
+ );
+});
+
+// pdfParser.loadPDF("./pdf2json/test/pdf/fd/form/F1040EZ.pdf");
