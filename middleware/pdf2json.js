284a12c79a0687e0a1292ee1339c122346894b91
Justin Joshi <JustinJoshi@proton.me>
2025-07-10 10:31:37 -0400

    successfully added code that converts pdf to txt, then writes to txt file

--- middleware/pdf2json.js
+++ middleware/pdf2json.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
 const PDFParser = require("pdf2json");
 
-const pdfParser = new PDFParser();
+const pdfParser = new PDFParser(this, 1);
 
 pdfParser.on("pdfParser_dataError", (errData) =>
  console.error(errData.parserError)
 );
 pdfParser.on("pdfParser_dataReady", (pdfData) => {
  fs.writeFile(
-  "./temp/output.json",
-  JSON.stringify(pdfData),
-  (data) => console.log(data)
+  "./temp/output.txt",
+    pdfParser.getRawTextContent(),
+    () => {
+    console.log("PDF successfully converted to txt");
+  }
  );
 });
 
