929f427236c9010357661bacfd75071529bcdc82
JustinJoshi <JustinJoshi@proton.me>
2025-07-14 09:54:06 -0500

    successfully completed download functionality

--- routes/convert.js
+++ routes/convert.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 const postsController = require("../controllers/posts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
-const temp = require("../temp/temp.js")
+const temp = require("../temp/temp.js");
 
 //Main Routes - simplified for now
 router.get("/:id", ensureAuth, postsController.getConvert);
 router.get("/:id/downloadJSON", ensureAuth, function(req, res){
-    const file = "C:/Users/Asanam/Desktop/mvc/binary-upload-boom/temp/output.json";
+    const file = "C:/Users/Justin/Desktop/Class/mvc/binary-upload-boom/temp/output.json";
     res.download(file);
     console.log("Sending json to user");
 }); 
 router.get("/:id/downloadTXT", ensureAuth, function(req, res){
-    const file = "C:/Users/Asanam/Desktop/mvc/binary-upload-boom/temp/output.txt";
+    const file = "C:/Users/Justin/Desktop/Class/mvc/binary-upload-boom/temp/output.txt";
     res.download(file);
     console.log("Sending txt to user");
 });
