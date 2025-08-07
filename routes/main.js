ca3dadd214c7d428248a86b58da2a3cddd26f3f3
JustinJoshi <JustinJoshi@proton.me>
2025-08-07 07:16:44 +0000

    new commit

--- routes/main.js
+++ routes/main.js
@@ -15,4 +15,4 @@ router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 
-module.exports = router;
+module.exports = router; 
